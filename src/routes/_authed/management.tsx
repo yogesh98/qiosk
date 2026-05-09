2855eaeeb5c8106572d3b95c60d403501b1b643d
Yogesh Patel <yogeshpatel@Yogeshs-MacBook-Pro.local>
2026-05-09 15:04:54 -0400

    nuked

--- src/routes/_authed/management.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { ManagementLayout } from '@/components/pages/management/ManagementLayout'
-
-export const Route = createFileRoute('/_authed/management')({
-  component: ManagementLayout,
-})
--- src/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { LandingPage } from "@/components/pages/landing";
-
-export const Route = createFileRoute("/")({ component: App });
-
-function App() {
-  return <LandingPage />;
-}
